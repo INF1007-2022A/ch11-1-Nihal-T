c602b762639f8297c085d7da07df6d98829cb56b
Nihal-T <112827282+Nihal-T@users.noreply.github.com>
2022-12-05 19:42:34 -0500

    adfae

--- game.py
+++ game.py
@@ -18,8 +18,24 @@ class Weapon:
 	:param power: Le niveau d'attaque
 	:param min_level: Le niveau minimal pour l'utiliser
 	"""
-
 	UNARMED_POWER = 20
+	def __init__(self,name,power, min_level) -> None:
+		self.__name=name
+		self.power =power
+		self.min_level=min_level
+
+	@property
+	def name(self):
+		return self.__name
+
+	@classmethod
+	def make_unarmed(cls):
+		return cls("Unarmed",cls.UNARMED_POWER,1)
+
+
+
+
+	
 
 
 class Character:
@@ -32,7 +48,21 @@ class Character:
 	:param defense: Le niveau de défense du personnage
 	:param level: Le niveau d'expérience du personnage
 	"""
+	def __init__(self, name, max_hp, attack, defense, level) -> None:
+		self.__name = name
+		self.max_hp = max_hp
+		self.attack = attack
+		self.defense = defense
+		self.level = level
+		self.weapon = None
+		self.hp = max_hp
 
+	@property
+	def name(self):
+		return self.__name
+
+	def compute_damage():
+		pass
 
 def deal_damage(attacker, defender):
 	# TODO: Calculer dégâts
